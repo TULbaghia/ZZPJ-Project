ba95f9d5d03bcaf13a6fe66878cfea67378985ac
Paweł Guzek <224304@edu.p.lodz.pl>
2021-04-25 14:26:23 +0200

    circleci, heroku, example endpoint

--- src/main/java/pl/lodz/p/it/zzpj/HelloApi.java
+++ src/main/java/pl/lodz/p/it/zzpj/HelloApi.java
@@ -1,2 +1,12 @@
-package pl.lodz.p.it.zzpj;public class HelloApi {
+package pl.lodz.p.it.zzpj;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloApi {
+    @GetMapping("/api/ping")
+    public String ping() {
+        return "pong";
+    }
 }
