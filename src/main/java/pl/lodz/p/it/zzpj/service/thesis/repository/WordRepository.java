4ae8e0b2fba1dce966f3deedce94833e3485fefd
Paweł Guzek <224304@edu.p.lodz.pl>
2021-06-11 10:43:58 +0200

    Change propagation; Change query to name parameterized #3

--- src/main/java/pl/lodz/p/it/zzpj/service/thesis/repository/WordRepository.java
+++ src/main/java/pl/lodz/p/it/zzpj/service/thesis/repository/WordRepository.java
@@ -2,6 +2,7 @@ package pl.lodz.p.it.zzpj.service.thesis.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
@@ -13,6 +14,6 @@ import pl.lodz.p.it.zzpj.exception.AppBaseException;
 @Transactional(propagation = Propagation.MANDATORY, isolation = Isolation.READ_COMMITTED, rollbackFor = AppBaseException.class)
 public interface WordRepository extends JpaRepository<Word, Long> {
 
-    @Query("SELECT w FROM Word w WHERE w.word = ?1")
-    Word findByWord(String word);
+    @Query("SELECT w FROM Word w WHERE w.word = :word")
+    Word findByWord(@Param("word") String word);
 }
